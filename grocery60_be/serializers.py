22e0e4011763cf1c98ddf9529440c317f26e6fc6
Santosh <>
2020-06-29 11:16:28 +0530

    changed meta and formatting for serializer

--- grocery60_be/serializers.py
+++ grocery60_be/serializers.py
@@ -14,72 +14,60 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'name']
 
 class StoreSerializer(serializers.ModelSerializer):
-	        model = Store
-	        fields = '__all__'
+    class Meta:
+        model = Store
+        fields = '__all__'
 
 class CustomerSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = Customer
-	        fields = '__all__'
+    class Meta:
+        model = Customer
+        fields = '__all__'
 
 class CartSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = Cart
-	        fields = '__all__'
+    class Meta:
+        model = Cart
+        fields = '__all__'
 
 class CatalogSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = Product
-	        fields = '__all__'
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 class BillingAddressSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = BillingAddress
-	        fields = '__all__'
+    class Meta:
+        model = BillingAddress
+        fields = '__all__'
 
 class ShippingAddressSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        db_table = 'shippingaddress'
-	        model = ShippingAddress
-	        fields = '__all__'
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = Order
-	        fields = '__all__'
+    class Meta:
+        model = Order
+        fields = '__all__'
 
 class OrderItemSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = OrderItem
-	        fields = '__all__'
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
 
 class OrderPaymentSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = OrderPayment
-	        fields = '__all__'
+    class Meta:
+        model = OrderPayment
+        fields = '__all__'
 
 class ShippingMethodSerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = ShippingMethod
-	        fields = '__all__'
+    class Meta:
+        model = ShippingMethod
+        fields = '__all__'
 
 class DeliverySerializer(serializers.ModelSerializer):
-	    class Meta:
-	        model = Delivery
-	        fields = '__all__'
-	        
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email', 'mobile', 'password')
+        model = Delivery
+        fields = '__all__'
 
-    def create(self, validated_data):
-        user = super(UserSerializer, self).create(validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
 
 
         
