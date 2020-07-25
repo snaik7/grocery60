48c1fdc0be26b9fada5c1988ed818abd688bf3a9
Santosh <>
2020-07-25 18:21:14 +0530

    Deactivation of store added

--- grocery60_be/models.py
+++ grocery60_be/models.py
@@ -44,6 +44,9 @@ class Store(models.Model):
     phone = models.CharField(
         max_length=50
     )
+    status = models.CharField(
+        max_length=20
+    )
 
     class Meta:
         db_table = "store"
