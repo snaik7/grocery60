3543339c2408775c869e94995fe5a37d87554a7d
Santosh <>
2020-07-27 12:52:23 +0530

    Change password

--- grocery60_be/settings.py
+++ grocery60_be/settings.py
@@ -201,7 +201,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@tissatech.com'
+EMAIL_HOST_USER = 'no-reply@grocery60.online'
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 GRAPHENE = {
