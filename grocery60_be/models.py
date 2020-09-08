51f559fd8f74742d91e3ebe791c74530a16a365b
Santosh <>
2020-09-08 19:30:08 +0530

    cart delete failed

--- grocery60_be/models.py
+++ grocery60_be/models.py
@@ -450,9 +450,9 @@ class OrderPayment(models.Model):
     def delete_cart(self, data):
         try:
             cart = None
-            customer_id = data.get('metadata').get('customer_id')
+            customer_id = data.get('customer_id')
             if not customer_id:
-                customer_id = data.get('customer_id')
+                customer_id = data.get('metadata').get('customer_id')
             cart = Cart.objects.get(customer_id=customer_id)
             if cart:
                 cart.delete()
